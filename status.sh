1b25f25e8c7bbfd42a71ec18ef6dba0721c9a024
indian954 <hiteshkardam.hk@gmail.com>
2019-07-25 02:58:47 +0530

    Add comments, new battery icon logic and show ip address

--- status.sh
+++ status.sh
@@ -2,26 +2,36 @@
 # You should see changes to the status bar after saving this script.
 # If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
 
-# Returns date in 12hr
+# Returns date and time in 12hr format.
 date_formatted=$(date +'%d-%m-%Y %l:%M:%S %p')
 
-# Returns battery percent
+# Returns battery percent.
 bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)
 
-# Returns the battery status: "Full", "Discharging", or "Charging".
-bat_status=$(cat /sys/class/power_supply/BAT0/status)
+# Returns 1 if charger connected and if disconnected.
+chrgr_status=$(cat /sys/class/power_supply/ADP1/online)
 
-if [ $bat_status == "Discharging" ]
+# Check if charger disconnected and use apt emoji.
+if [ $chrgr_status -eq "0" ]
 then
 	bat_emoji="🔋 "
 else
 	bat_emoji="⚡"
 fi
 
+# Returns SSID of connected wifi.
 wifi_ssid=$(nmcli d wifi | grep "^\*" | grep -v "\*.*SSID" | awk '{ print $2 }')
 
+# Returns signal strength 0-100.
 wifi_str=$(nmcli d wifi | grep "^\*" | grep -v "\*.*SSID" | awk '{ print $7 }')
 
+# Assign ip if only lan/wifi connected.
+if [ $(hostname -i) != "127.0.0.2" ]
+then
+	my_ip=$(hostname -i)
+fi
+
+# Emojify wifi signal strength.
 if [ $wifi_str -le 25 ]
 then
 	wifi_symbol="▂___"
@@ -42,5 +52,6 @@ else
 	wifi_symbol="WIFI NOT CONNECTED"
 
 fi	
-	
-echo │ $wifi_ssid $wifi_symbol │ $bat_emoji$bat_percent% │ $date_formatted │
+
+# Statusbar print
+echo │ $my_ip $wifi_ssid $wifi_symbol │ $bat_emoji$bat_percent% │ $date_formatted │
