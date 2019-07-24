ba32c6f2c4028d4611030e53eb14267c22e1b150
indian954 <hiteshkardam.hk@gmail.com>
2019-07-25 05:00:39 +0530

    Install termite-nocsd instead, add unlimited scrollback for termite and add comments

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 
 import os
 
-os.system("yay -Syu && yay -S sway swayidle swaylock dmenu screenfetch vim xorg-server-xwayland nautilus grim wl-clipboard nomacs termite light google-chrome ")
+os.system("yay -Syu && yay -S sway swayidle swaylock dmenu screenfetch vim xorg-server-xwayland nautilus grim wl-clipboard nomacs termite-nocsd light google-chrome ")
 
-filebashrc = os.environ['HOME'] + "/" + ".bashrc"
+# Paths
+homepath = os.environ['HOME'] + "/" 
+filebashrc = homepath + ".bashrc"
+termitedir = homepath + ".config/termite"
 
+# Autostart sway if logging in from tty1
 with open(filebashrc, 'r') as f:
     if "exec sway" not in f.read():
         f.close()
@@ -16,7 +20,13 @@ if [ "$(tty)" = "/dev/tty1" ]; then
 fi""")
             g.close()
 
+# Termite infite scrollback
+if os.path.exists(homepath+".config/termite") == False:
+    os.system("mkdir $HOME/.config/termite")
+    os.system("touch $HOME/.config/termite/config")
+    os.system("echo scrollback_lines = -1 > $HOME/.config/termite/config")
 
+# Fancy, eh!
 print("\n############################")
 print("### ALL SET UP FOR SWAY! ###")
 print("############################\n")
